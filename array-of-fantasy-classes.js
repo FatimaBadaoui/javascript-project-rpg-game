412294800d7c816c147edc61f5e1da75cc709569
Fatima Badaoui <fatimina615@gmail.com>
2023-12-01 11:23:45 +0100

    add new file for fantasyClasses array

---
+++ array-of-fantasy-classes.js
@@ -0,0 +1,134 @@
+export const fantasyClasses = [
+  {
+    className: "Mage",
+    attackingSkills: [
+      "Fireball",
+      "Ice Lance",
+      "Thunderstorm",
+      "Arcane Missile",
+      "Teleport Strike",
+      "Time Warp",
+    ],
+  },
+  {
+    className: "Warrior",
+    attackingSkills: [
+      "Sword Slash",
+      "Shield Bash",
+      "Whirlwind Strike",
+      "Charge",
+      "Rending Strike",
+      "Execute",
+    ],
+  },
+  {
+    className: "Rogue",
+    attackingSkills: [
+      "Backstab",
+      "Shadowstep",
+      "Poison Dagger",
+      "Eviscerate",
+      "Stealth Ambush",
+      "Distract",
+    ],
+  },
+  {
+    className: "Archer",
+    attackingSkills: [
+      "Rapid Shot",
+      "Piercing Arrow",
+      "Explosive Arrow",
+      "Snipe",
+      "Volley",
+      "Eagle Eye",
+    ],
+  },
+  {
+    className: "Paladin",
+    attackingSkills: [
+      "Divine Strike",
+      "Holy Smite",
+      "Shield of Faith",
+      "Avenging Wrath",
+      "Lay on Hands",
+      "Consecration",
+    ],
+  },
+  {
+    className: "Sorcerer",
+    attackingSkills: [
+      "Magic Missile",
+      "Summon Elemental",
+      "Chain Lightning",
+      "Mind Control",
+      "Polymorph",
+      "Time Freeze",
+    ],
+  },
+  {
+    className: "Bard",
+    attackingSkills: [
+      "Melodic Strike",
+      "Inspiring Anthem",
+      "Disorienting Tune",
+      "Enchanting Serenade",
+      "Sonic Wave",
+      "Harmony Blast",
+    ],
+  },
+  {
+    className: "Druid",
+    attackingSkills: [
+      "Nature's Wrath",
+      "Wild Shape",
+      "Entangling Vines",
+      "Moonfire",
+      "Sunfire",
+      "Starfall",
+    ],
+  },
+  {
+    className: "Necromancer",
+    attackingSkills: [
+      "Death Grip",
+      "Raise Undead",
+      "Dark Ritual",
+      "Soul Drain",
+      "Bone Spike",
+      "Cursed Nova",
+    ],
+  },
+  {
+    className: "Monk",
+    attackingSkills: [
+      "Fist of Fury",
+      "Zen Strike",
+      "Rolling Thunder",
+      "Mystic Palm",
+      "Dragon Kick",
+      "Whirlwind Staff",
+    ],
+  },
+  {
+    className: "Assassin",
+    attackingSkills: [
+      "Silent Strike",
+      "Toxic Blade",
+      "Shadow Cloak",
+      "Assassinate",
+      "Smoke Bomb",
+      "Venomous Stab",
+    ],
+  },
+  {
+    className: "Beastmaster",
+    attackingSkills: [
+      "Summon Beast Companion",
+      "Feral Swipe",
+      "Roar of the Wild",
+      "Predator's Pounce",
+      "Savage Maul",
+      "Beastial Fury",
+    ],
+  },
+];
